cc476ee7f43ab62a7fb46e2c3ab3f23310f19205
Alihaitham26 <alihaithamahmed26@gmail.com>
2022-09-22 11:46:23 +0200

    add Piece class

---
+++ script/Piece.js
@@ -0,0 +1,17 @@
+class Piece{
+    static types=["pawn","rook","knight","bishop","queen","king"]
+    constructor(type,isWhite){
+        if(!Piece.types.includes(type)){
+            throw new Error(`${type} is not a piece type`)
+        }else if(typeof(isWhite) !== "boolean"){
+            throw new Error("isWhite must boolean")
+        }
+        this.type=type
+        this.isWhite=isWhite
+        this.positionsLog=[]
+        this.img=`${isWhite?"w":"b"}_${type}.png`
+    }
+    move(position){
+        this.positionsLog.push(position)
+    }
+}
