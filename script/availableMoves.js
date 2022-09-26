446b4702e6cafe6dd5b57a763fb538b1de58600e
Alihaitham26 <alihaithamahmed26@gmail.com>
2022-09-26 14:10:07 +0200

    add queen,bishop and rook available moves

--- script/availableMoves.js
+++ script/availableMoves.js
@@ -1,19 +1,32 @@
 function getAvailableMoves(boardMap, position) {
+    let getMovesArray=(xDirection,yDirection)=>{
+        //function to move in x or/and y untill find peice so that stop and return moves
+        let x=xDirection
+        let y=yDirection
+        let moves=[]
+        let nextMove=moveOnBoard(position,x,y)
+        while(nextMove&&!boardMap[nextMove]){
+            moves.push(nextMove)
+            x+=xDirection
+            y+=yDirection
+            nextMove=moveOnBoard(position,x,y)
+        }
+        return moves
+    }
     let piece= boardMap[position]
     let type = piece.type
     let availableMoves=[]
     switch (type) {
         case "pawn":
             let yDeriction=boardMap[position].isWhite?1:-1
-            
             let front=moveOnBoard(position,0,yDeriction)
             let superFront
             if(piece.movesLog.length===0&&piece.movesLog.length===0){
                 superFront=moveOnBoard(position,0,yDeriction*2)
             }
-            if(!boardMap[front]){
+            if(front&&!boardMap[front]){
                 availableMoves.push(front)
-                if(!boardMap[superFront]){
+                if(superFront&&!boardMap[superFront]){
                     availableMoves.push(superFront)
                 }
             }
@@ -25,9 +38,6 @@ function getAvailableMoves(boardMap, position) {
             if(boardMap[right]&&boardMap[right].isWhite!==piece.isWhite){
                 availableMoves.push(right)
             }
-            
-        case "rook":
-            console.log(type)
             break
         case "knight":
             let moves=[
@@ -39,17 +49,16 @@ function getAvailableMoves(boardMap, position) {
                 moveOnBoard(position,1,-2),
                 moveOnBoard(position,-1,2),
                 moveOnBoard(position,-1,-2)]
-            moves.map(move=>{
-                if(move&&!boardMap[move]){
-                    availableMoves.push(move)
-                }
-            })
+            moves.filter(move=>move&&!boardMap[move]).forEach(move=>{availableMoves.push(move)})
+            break
+        case "rook":
+            availableMoves=[...getMovesArray(1,0),...getMovesArray(0,1),...getMovesArray(-1,0),...getMovesArray(0,-1)]
             break
         case "bishop":
-            console.log(type)
+            availableMoves=[...getMovesArray(1,1),...getMovesArray(-1,-1),...getMovesArray(-1,1),...getMovesArray(1,-1)]
             break
         case "queen":
-            console.log(type)
+            availableMoves=[...getMovesArray(1,1),...getMovesArray(-1,-1),...getMovesArray(-1,1),...getMovesArray(1,-1),...getMovesArray(1,0),...getMovesArray(0,1),...getMovesArray(-1,0),...getMovesArray(0,-1)]
             break
         case "king":
             console.log(type)
